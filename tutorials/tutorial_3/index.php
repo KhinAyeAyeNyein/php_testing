8b9dc3ba378e108ddf4ecbf9c7d30cb0a91e6dcc
KhinAyeAyeNyein <kaan.miit@gmail.com>
2021-10-15 16:12:31 +0630

    [master] modified php function

--- tutorials/tutorial_3/index.php
+++ tutorials/tutorial_3/index.php
@@ -12,7 +12,7 @@
     <div class='body'>
         <form>
             <div class='input'>
-            <input type="date" name="dob" value="<?php echo (isset($_GET['dob']))?$_GET['dob']:'';?>">                
+            <input type="date" name="dob" value="<?php echo (isset($_GET['dob'])) ?$_GET['dob']:'';?>">                
             <input type="submit" value="Calculate Age">
             </div>
         </form>
@@ -28,13 +28,13 @@
 <?php
 function getAge($dob){
   $b_day = new DateTime($dob);
-  $today = new DateTime(date('m.d.y'));
+  $today = new DateTime(date('d.m.y'));
   if ($b_day > $today){
     return 'You are not born yet.';
   }
   else {
     $age = $today->diff($b_day);
-    return 'Your Age is : '.$age->y.' Years, '.$age->m.' month, and '.$age->d.' days.';
+    return 'Your age is : '.$age->d.' days, '.$age->m.' month, and '.$age->y.' years.';
   }
 }
 ?>
